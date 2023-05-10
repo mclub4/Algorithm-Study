19a98786992d198729fb5fac3b30bec594c7046d
mclub4 <mclub901@gmail.com>
2023-05-11 00:22:10 +0900

    알고리즘 정리

---
+++ Union&Find/집합의 표현.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws  IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n+1];
+
+        for(int i = 0; i<n+1; i++){
+            parent[i] = i;
+        }
+
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int check = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(check == 0){
+                combine(a,b);
+            }
+            else{
+                boolean result = iscombined(a, b);
+                if(result) System.out.println("YES");
+                else System.out.println("NO");
+            }
+        }
+     }
+
+    public static int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public static void combine(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if(parent_a != parent_b){
+            if (parent_a < parent_b) {
+                parent[parent_b] = parent_a;
+            } else {
+                parent[parent_a] = parent_b;
+            }
+        }
+    }
+
+    public static boolean iscombined(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if(parent_a == parent_b) return true;
+        else return false;
+    }
+
+}
